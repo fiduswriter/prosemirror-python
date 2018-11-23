ed3375ddb068891621c1d2e769197e84e9de44b2
Johannes Wilm <mail@johanneswilm.org>
2018-11-23 23:22:46 +0100

    release 0.0.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='prosemirror-python',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'Js2Py==0.59',
